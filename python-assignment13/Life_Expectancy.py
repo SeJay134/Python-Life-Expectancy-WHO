094b27e0539d618b16b11445cbd4f9e0c6fcead1
SeJay134 <patrushev.s.job@gmail.com>
2026-02-12 15:02:31 -0500

    setup

--- python-assignment13/Life_Expectancy.py
+++ python-assignment13/Life_Expectancy.py
@@ -6,3 +6,35 @@ from pathlib import Path
 import streamlit as st
 import plotly.express as px
 
+# Setup
+st.set_page_config(
+    page_title="Life Expectancy (WHO)",
+    page_icon="🧬",
+    layout="wide"
+)
+
+# The way to the current scripts
+BASE_DIR = Path(__file__)
+
+# Read CSV
+csv_path = BASE_DIR.parent / "db" / "Life-Expectancy-Data.csv"
+
+# Data
+db = pd.read_csv(csv_path)
+# print(" \n", db.head())
+# print()
+
+# The Life Expectancy (WHO) dataset was loaded directly from Kaggle. 
+# The dataset contains country-level health, economic, and demographic indicators collected over multiple years.
+
+# checking
+# print("--columns--")
+# print(db.columns)
+# print()
+
+# print("--info--")
+# db.info()
+# print()
+
+# No additional data type conversion was required, as all numerical and categorical
+# features were already stored in appropriate formats.
